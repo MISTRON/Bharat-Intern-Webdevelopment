15f20d920e23d3766028da3b16c4601a11523e2b
Piyush Singh Bhati <148543843+MISTRON@users.noreply.github.com>
2024-01-31 05:36:52 +0530

    Update script.js

--- Portfolio Website/script.js
+++ Portfolio Website/script.js
@@ -47,7 +47,7 @@ const showMenu = (toggleId, navId) => {
   /*===== COPY Email =====*/
   const copy = document.getElementById("copy");
   copy.addEventListener("click", () => {
-    navigator.clipboard.writeText("kasulaarunteja@gmail.com");
+    navigator.clipboard.writeText("bhatipiyush40@gmail.com");
     copy.innerHTML = "copied";
     setTimeout(() => {
       copy.innerHTML = null;
